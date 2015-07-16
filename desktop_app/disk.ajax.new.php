91503f016f732f40e04721d1333e9dceef8d4421
Plotnikov Andrey <dewww.pl@gmail.com>
2015-07-16 20:47:09 +0300

    Delete not used folders

--- desktop_app/disk.ajax.new.php
+++
@@ -1,3 +0,0 @@
-<?
-require($_SERVER["DOCUMENT_ROOT"]."/bitrix/components/bitrix/disk.bitrix24disk/ajax.php");
-?>
--- urlrewrite.php
+++ urlrewrite.php
@@ -19,12 +19,6 @@ $arUrlRewrite = array(
 		"PATH" => "/catalog/index.php",
 	),
 	array(
-		"CONDITION" => "#^/store/#",
-		"RULE" => "",
-		"ID" => "bitrix:catalog.store",
-		"PATH" => "/store/index.php",
-	),
-	array(
 		"CONDITION" => "#^/news/#",
 		"RULE" => "",
 		"ID" => "bitrix:news",
